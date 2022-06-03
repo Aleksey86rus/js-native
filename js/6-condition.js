a412e133af8ab48e4669de0cf25f5f16b8c4beee
Aleksey86rus <Coppola80@mail.ru>
2022-06-03 20:54:24 +0500

    add 6-homework.js

--- js/6-condition.js
+++ js/6-condition.js
@@ -40,10 +40,10 @@
 // Пользователь должен попасть в какой-то промежуток
 // От 1 до 10 вкл [1; 10]
 
-let numUser = 0;
-if (numUser >= 1 && numUser <= 10) {
-  console.log('Win!');
-}
+// let numUser = 0;
+// if (numUser >= 1 && numUser <= 10) {
+//   console.log('Win!');
+// }
 
 
 
@@ -52,16 +52,16 @@ if (numUser >= 1 && numUser <= 10) {
 // От 1 до 7 не вкл [-5; 7)
 
 
-// let numUser2 = 7;
+// let numUser2 = 6;
 // if (numUser2 >= -5 && numUser2 < 7) {
 //   console.log('Win!');
 // }
 
 // Надо не попасть в промежуток от 5 до 15 вкл
-let luck = 10;
-if (luck < 5 || luck > 15) {
-  console.log('luck');
-}
+// let luck = 10;
+// if (luck < 5 || luck > 15) {
+//   console.log('luck');
+// }
 
 // let luck = 10;
 // if (!(luck >= 5 && luck <= 15)) {
@@ -69,28 +69,28 @@ if (luck < 5 || luck > 15) {
 // }
 
 // * Проверка на чётность (делимость)
-let testNum = 22;
-if (testNum % 2 === 0) {
-  console.log('even');
-} else {
-  console.log('odd');
-}
+// let testNum = 22;
+// if (testNum % 2 === 0) {
+//   console.log('even');
+// } else {
+//   console.log('odd');
+// }
 
 // * Проверка делится ли число на 177?
 
-let testNumber = 355;
-if (testNumber % 177 === 0) {
-  console.log('Делится без остатка');
-} else {
-  console.log('Не делится без остатка');
-}
+// let testNumber = 355;
+// if (testNumber % 177 === 0) {
+//   console.log('Делится без остатка');
+// } else {
+//   console.log('Не делится без остатка');
+// }
 
 
 // * Проверка на нечётность (делимость)
-let testNum2 = 22;
-if (testNum2 % 2 !== 0) {
-  console.log('odd');
-}
+// let testNum2 = 22;
+// if (testNum2 % 2 !== 0) {
+//   console.log('odd');
+// }
 // !(20-22)
 // 1-19 вкл
 // 18
